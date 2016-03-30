dc81f3a6677b907ce2a90424df5cf6fed8eff25c
Paul Molin <paulmp.molin@gmail.com>
2016-03-30 14:02:54 +0200

    Add content for main pages

--- mjmr/src/AppBundle/Controller/DefaultController.php
+++ mjmr/src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,54 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="default_index")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/nos-fiancailles", name="default_engagement")
+     */
+    public function engagementAction()
+    {
+        return $this->render('default/engagement.html.twig');
+    }
+
+    /**
+     * @Route("/mariage-civil", name="default_civil_marriage")
+     */
+    public function civilMarriageAction()
+    {
+        return $this->render('default/civilMarriage.html.twig');
+    }
+
+    /**
+     * @Route("/messe-de-mariage", name="default_nuptial_mass")
+     */
+    public function nuptialMassAction()
+    {
+        return $this->render('default/nuptialMass.html.twig');
+    }
+
+    /**
+     * @Route("/reception", name="default_reception")
+     */
+    public function receptionAction()
+    {
+        return $this->render('default/reception.html.twig');
+    }
+
+    /**
+     * @Route("/reception", name="default_wedding_list")
+     */
+    public function weddingListAction()
+    {
+        return $this->render('default/weddingList.html.twig');
     }
 }
