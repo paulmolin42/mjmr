804f7d7f1a7bed9fc49306f69fa0b487ca019767
Paul Molin <paulmp.molin@gmail.com>
2016-04-10 22:58:55 +0200

    Share images files

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,4 +7,7 @@ set :repo_url, 'git@github.com:paulmolin42/mjmr.git'
 set :tmp_dir, '/var/www/tmp'
 set :deploy_to, '/var/www/mjmr'
 
+set :linked_files, ['app/config/parameters.yml']
+set :linked_dirs, [fetch(:log_path), 'web/images']
+
 after 'deploy:updated', 'npm:install'
